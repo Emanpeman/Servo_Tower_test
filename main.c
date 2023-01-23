25b0b73c2608b6d12179ddff5eaf712521d55133
Emanuela Tuong Vi Thi Tran <ettran@ntnu.no>
2023-01-23 17:08:13 +0100

    finished SDK HAL and FreeRTOS setup

--- main.c
+++ main.c
@@ -9,29 +9,58 @@ File    : main.c
 Purpose : Generic application start
 
 */
+
+#include <nrf.h>
+#include <hal/nrf_gpio.h>
 #include "FreeRTOS.h"
 #include "task.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 
+#define Button_2 12
+#define Button_4 25
+#define LED_2 14
+#define LED_4 16
 
-/*********************************************************************
-*
-*       main()
-*
-*  Function description
-*   Application entry point.
-*/
+
 int main(void) {
-  int i;
+ 
 
-  for (i = 0; i < 100; i++) {
-    printf("Hello World %d!\n", i);
-  }
-  do {
-    i++;
-  } while (1);
+ nrf_gpio_cfg_output(LED_2);
+ nrf_gpio_cfg_output(LED_4);
+ nrf_gpio_cfg_input(Button_2, NRF_GPIO_PIN_PULLUP);
+ nrf_gpio_cfg_input(Button_4, NRF_GPIO_PIN_PULLUP);
+
+
+ nrf_gpio_pin_set(LED_2);
+ nrf_gpio_pin_set(LED_4);
+
+while(1)
+{
+
+
+
+
+if(nrf_gpio_pin_read(Button_2) == 0)
+nrf_gpio_pin_clear(LED_2); 
+
+else
+nrf_gpio_pin_set(LED_2);
+
+
+if(nrf_gpio_pin_read(Button_4) == 0)
+nrf_gpio_pin_clear(LED_4); 
+
+else
+nrf_gpio_pin_set(LED_4);
+
+
+
+
 }
 
-/*************************** End of file ****************************/
+
+  }
+
+  /*************************** End of file ****************************/
